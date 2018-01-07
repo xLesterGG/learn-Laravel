0680aa3b53ebf3e4632401a459ca981d26f482bd
Lester G <Lester G>
2018-01-07 10:47:58 +0800

    moving controller logic from routes file to controller

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,21 @@
 |
 */
 
-Route::get('/tasks', function () {
-    // $tasks = DB::table('tasks')->get();
-    $tasks = App\Task::all();
+Route::get('/tasks','TasksController@index');
 
-    return view('tasks.index',compact('tasks'));
-});
+Route::get('/tasks/{task}','TasksController@show');
 
-Route::get('/tasks/{task}', function ($id) {
 
-    // $task = DB::table('tasks')->find($id);
-    $task = App\Task::find($id);
+// Route::get('/tasks', function () {
+//     // $tasks = DB::table('tasks')->get();
+//     $tasks = App\Task::all();
+//     return view('tasks.index',compact('tasks'));
+// });
 
-    return view('tasks.show',compact('task'));
-});
+// Route::get('/tasks/{task}', function ($id) {
+//
+//     // $task = DB::table('tasks')->find($id);
+//     $task = App\Task::find($id);
+//
+//     return view('tasks.show',compact('task'));
+// });
